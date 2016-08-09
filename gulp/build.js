7e2976dca8043645d2852d77d5636f3976fcb131
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-08-10 01:43:03 +0300

    fix bild.js for matching fonts

--- gulp/build.js
+++ gulp/build.js
@@ -69,8 +69,7 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-  return gulp.src($.mainBowerFiles())
-    .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
+  return gulp.src('/assets/fonts/**/*.*{eot,svg,ttf,woff,woff2,otf}')
     .pipe($.flatten())
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
 });
